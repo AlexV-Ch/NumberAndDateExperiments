243da9fdd598424d2f96866a2257b1e9e3043415
AlexV-Ch <alexch@yandex.ru>
2025-01-18 21:46:44 +0300

    8.2 Операции с числами задание 1 начало

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+    MoneyCalculator casset1 = new MoneyCalculator();
+        System.out.println(casset1.calculateCassetteCount(1999));
+        System.out.println(casset1.calculateCassetteCount(2000));
+        System.out.println(casset1.calculateCassetteCount(2001));
+        System.out.println(casset1.calculateCassetteCount(2500));
+
+    }
+}
