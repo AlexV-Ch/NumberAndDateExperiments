a57dd931098cb085e73be1296cda45f887bec448
AlexV-Ch <alexch@yandex.ru>
2025-01-20 19:55:16 +0300

    8.2 Операции с числами задание 1 готово

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,12 @@ public class Main {
         System.out.println(casset1.calculateCassetteCount(4001));
         // По рекомендации
         System.out.println(MoneyCalculator.calculateCassetteCount(5000));
+// Задание 8.3 Инкремент и декремент
+        UsageAnalytics analytics = new UsageAnalytics();
+        analytics.countVisit();
+        analytics.countVisit();
+        int visits = analytics.countVisit();
+        System.out.println("Всего посещений: " + visits);
 
     }
 }
---
+++ src/UsageAnalytics.java
@@ -0,0 +1,15 @@
+
+// Задание 8.3 Инкремент и декремент
+public class UsageAnalytics {
+    private int visitsCount;
+
+    public UsageAnalytics() {
+        visitsCount = 0;
+    }
+
+    public int countVisit() {
+        visitsCount++; // или просто - return ++visitsCount;
+        return visitsCount;
+    }
+
+}
